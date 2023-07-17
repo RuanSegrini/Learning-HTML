26109f7686165d18003566c3b48e3ca0bcc212a9
Ruan Carolino Segrini <102704092+RuanSegrini@users.noreply.github.com>
2023-07-17 19:47:33 -0300

    Create formataçoes.java

---
+++ Estruturas/formataçoes.java
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="Iconarchive-Dog-Breed-Bernese-Mountain-Dog.512.png" type="image/x-icon">
+    <title>Hierarquia de Titulos</title>
+    <style>
+        body{
+            background-color: rgb(255, 255, 255);
+        }
+    </style>
+</head>
+<body>
+   <h1>Principais formataçoes</h1>
+   <h2>Negrito / Destaque</h2>
+   <p>Nesta frase temos um <b>termo em negrito</b> usando a tag B( nao semantica).</p>
+   <p>Nesta frase, tenho um termo em <strong>termo em destaque</strong> usando a tag STRONG (semantica)</p>
+   <h2>Italico / Enfase</h2>
+   <p>Nesta frase, temos um <i>termo em italico</i> usando a tag I(nao semantica)</p>
+   <p>Nesta frase, temos um <i>termo em enfase</i> usando a tag EM(semantica)</p>
+   <h2>Texto marcado</h2>
+   <p>podemos criar tambem <mark>um texto marcado</mark></p>
+   <h2>Texto grande e pequeno</h2>
+   <p>estamos criando um texto grande e um <small>texto pequeno</small>></p>
+   
+</body>
+</html>
